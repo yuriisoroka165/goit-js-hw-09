6479141359b8e4fc0ceb9c5cc01789b1bf4a74eb
Yurii Soroka <yurisoroka165@gmail.com>
2023-02-01 17:23:49 +0200

    task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const refs = {
+  siteForm: document.querySelector('.form'),
+}
+refs.siteForm.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const data = [];
+  formData.forEach((value) => {
+    data.push(value);
+  });
+  let delay = Number(data[0]);
+  for (let i = 1; i <= Number(data[2]); i++) {
+    createPromise(i, delay)
+      .then(onResolve)
+      .catch(onReject);
+    delay += Number(data[1]);
   }
+}
+
+function onResolve({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+}
+
+function onReject({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
 }
